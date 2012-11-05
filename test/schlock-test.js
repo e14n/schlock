2e3647cb6c7d640c4ec168f55dbd316b86103e2f
Evan Prodromou <evan@status.net>
2012-11-05 10:48:37 -0500

    Test slipping a writer between readers

--- test/schlock-test.js
+++ test/schlock-test.js
@@ -91,13 +91,14 @@ suite.addBatch({
                         function(err) {
                             if (err) throw err;
                             resource2 = 16;
+                            value = resource2;
                             schlock.writeUnlock("resource2", this);
                         },
                         function(err) {
                             if (err) {
                                 callback(err, null);
                             } else {
-                                callback(null, resource2);
+                                callback(null, value);
                             }
                         }
                     );
@@ -105,6 +106,134 @@ suite.addBatch({
                 "it works": function(err, value) {
                     assert.ifError(err);
                     assert.equal(value, 16);
+                }
+            },
+            "and we readLock a resource multiple times": {
+                topic: function(schlock) {
+                    var callback = this.callback,
+                        resource3 = 15;
+
+                    Step(
+                        function() {
+                            var i,
+                                group = this.group(),
+                                reader = function(cb) {
+                                    return function(err) {
+                                        var value;
+                                        if (err) {
+                                            cb(err, null);
+                                        } else {
+                                            value = resource3;
+                                            schlock.readUnlock("resource3", function(err) {
+                                                if (err) {
+                                                    cb(err, null);
+                                                } else {
+                                                    cb(null, value);
+                                                }
+                                            });
+                                        }
+                                    };
+                                };
+
+                            for (i = 0; i < 10; i++) {
+                                schlock.readLock("resource3", reader(group()));
+                            }
+                        },
+                        function(err, results) {
+                            if (err) {
+                                callback(err, null);
+                            } else {
+                                callback(null, results);
+                            }
+                        }
+                    );
+                },
+                "it works": function(err, results) {
+                    var i;
+                    assert.ifError(err);
+                    assert.isArray(results);
+                    assert.lengthOf(results, 10);
+                    for (i = 0; i < 10; i++) {
+                        assert.equal(results[i], 15);
+                    }
+                }
+            },
+            "and we do a writer between readers": {
+                topic: function(schlock) {
+                    var callback = this.callback,
+                        resource4 = 8;
+
+                    Step(
+                        function() {
+                            var i,
+                                group = this.group(),
+                                reader = function(cb) {
+                                    var value;
+                                    return function(err) {
+                                        if (err) {
+                                            cb(err, null);
+                                        } else {
+                                            value = resource4;
+                                            schlock.readUnlock("resource4", function(err) {
+                                                if (err) {
+                                                    cb(err, null);
+                                                } else {
+                                                    cb(null, value);
+                                                }
+                                            });
+                                        }
+                                    };
+                                },
+                                writer = function(cb) {
+                                    var value;
+                                    return function(err) {
+                                        if (err) {
+                                            cb(err, null);
+                                        } else {
+                                            resource4 = 4;
+                                            value = resource4;
+                                            schlock.writeUnlock("resource4", function(err) {
+                                                if (err) {
+                                                    cb(err, null);
+                                                } else {
+                                                    cb(null, value);
+                                                }
+                                            });
+                                        }
+                                    };
+                                };
+
+                            for (i = 0; i < 10; i++) {
+                                schlock.readLock("resource4", reader(group()));
+                            }
+
+                            schlock.writeLock("resource4", writer(group()));
+
+                            for (i = 11; i < 21; i++) {
+                                schlock.readLock("resource4", reader(group()));
+                            }
+                        },
+                        function(err, results) {
+                            if (err) {
+                                callback(err, null);
+                            } else {
+                                callback(null, results);
+                            }
+                        }
+                    );
+                },
+                "it works": function(err, results) {
+                    var i;
+                    assert.ifError(err);
+                    assert.isArray(results);
+                    assert.lengthOf(results, 21);
+                    for (i = 0; i < 10; i++) {
+                        assert.equal(results[i], 8);
+                    }
+                    assert.equal(results[10], 4);
+                    for (i = 11; i < 21; i++) {
+                        assert.equal(results[i], 4);
+                    }
                 }
             }
         }
